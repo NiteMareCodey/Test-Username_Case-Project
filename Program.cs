05ccef46d5b6fe2226ef81b19ceb8da2f25c9543
pilot <pilot@LAPTOP-2DT6KLAN>
2022-09-25 12:22:21 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test_Username_Case_Project
+{
+    class Program
+    {
+        private static string NoteDirectory =
+            Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) + @"\Notes";
+
+
+        static void Main(string[] args)
+        {
+
+        }
+    }   private static void ReadCommand()
+    {
+        Console.Write(Directory.GetDirectoryRoot(NoteDirectory));
+        string Command = Console.ReadLine();
+
+        switch (Command.ToLower()
+        {
+            case "new":
+                NewNote();
+                //Main(null);
+                break;
+            case "exit":
+                Exit();
+                break;
+            default:
+                CommandsAvailable();
+                //Main(null);
+                break;
+            case "edit":
+                EditNote();
+                //  Main(null);
+                break;
+        }
+    }
+
+    #region Command Response Methods
+    private static void NewNote()
+    {
+        Console.WriteLine("in NewNote");
+    }
+
+    private static void CommandsAvailable()
+    {
+        Console.WriteLine("Invalid Option");
+    }
+
+    private static void Exit()
+    {
+        Environment.Exit(0);
+    }
+
+    private static void EditNote()
+    {
+        Console.WriteLine("CHANGING NOTE");
+    }
+# endregion
+}
