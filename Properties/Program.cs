247b209951bce5537c9383a404c477a98521bb9c
NiteMareCodey <107191864+NiteMareCodey@users.noreply.github.com>
2022-09-26 18:03:40 -0400

    Add files via upload
    Here are the 2 other projects combined to form Test Username

---
+++ Properties/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test_Console_App
+{
+    internal class Program
+    {
+        public class car
+        {
+            public string color;
+           public void run()
+            {
+                Console.WriteLine("vroom");
+
+            }
+        }
+        static void Main(string[] args)
+        {
+            Mycar();
+            DoSomething();
+            
+
+        }
+
+        static void DoSomething()
+        {
+            Console.WriteLine("Enter Username:");
+            string username = Console.ReadLine();
+            Console.Write("Username is:" + username);
+            //Console.Readline();
+            Console.ReadKey();
+        }
+        static void Mycar()
+        {
+            car Beater=new car();
+            Beater.color = "Red";
+            Beater.run();
+        }
+    }
+}
+
